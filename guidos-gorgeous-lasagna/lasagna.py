a2a14a9f64242495df6f877b6f4ca4852031b905
nixbytes <real8686@gmail.com>
2023-08-30 22:02:53 -0400

    add initial code

--- guidos-gorgeous-lasagna/lasagna.py
+++ guidos-gorgeous-lasagna/lasagna.py
@@ -8,11 +8,12 @@ of a module and its functions and/or classes.
 """
 
 
-#TODO: define the 'EXPECTED_BAKE_TIME' constant.
+# TODO: define the 'EXPECTED_BAKE_TIME' constant.
+EXPECTED_BAKE_TIME = 40
 
 
-#TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
-def bake_time_remaining():
+# TODO: Remove 'pass' and complete the 'bake_time_remaining()' function below.
+def bake_time_remaining(actual_bake_time):
     """Calculate the bake time remaining.
 
     :param elapsed_bake_time: int - baking time already elapsed.
@@ -23,13 +24,39 @@ def bake_time_remaining():
     based on the `EXPECTED_BAKE_TIME`.
     """
 
-    pass
+    return int(EXPECTED_BAKE_TIME - actual_bake_time)
 
 
-#TODO: Define the 'preparation_time_in_minutes()' function below.
+# TODO: Define the 'preparation_time_in_minutes()' function below.
 # You might also consider using 'PREPARATION_TIME' here, if you have it defined.
+# Calculate preparation time in minutes
+def preparation_time_in_minutes(number_of_layers):
+    return number_of_layers * 2
 
 
-
-#TODO: define the 'elapsed_time_in_minutes()' function below.
+# TODO: define the 'elapsed_time_in_minutes()' function below.
 # Remember to add a docstring (you can copy and then alter the one from bake_time_remaining.)
+
+
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    prep_time = preparation_time_in_minutes(number_of_layers)
+    return prep_time + elapsed_bake_time
+
+
+# Instructions and notes
+def main():
+    # Calculate remaining bake time
+    remaining_bake = bake_time_remaining(30)
+    print(f"Remaining bake time: {remaining_bake} minutes")
+
+    # Calculate preparation time
+    prep_time = preparation_time_in_minutes(2)
+    print(f"Preparation time: {prep_time} minutes")
+
+    # Calculate total elapsed cooking time
+    total_time = elapsed_time_in_minutes(3, 20)
+    print(f"Total elapsed cooking time: {total_time} minutes")
+
+
+if __name__ == "__main__":
+    main()
